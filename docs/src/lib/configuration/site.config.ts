cd4e6e7b4a55c0a4a4356bfab1f41b6414a992ff
Brandon D <76827979+BranDenn@users.noreply.github.com>
2026-06-15 17:14:37 +0000

    remove trailing "/" at siteConfig.origin

--- docs/src/lib/configuration/site.config.ts
+++ docs/src/lib/configuration/site.config.ts
@@ -1,6 +1,6 @@
 const siteConfig = {
 	name: 'Godot-Router',
-	origin: 'https://godecosystem.github.io/godot-router/',
+	origin: 'https://godecosystem.github.io/godot-router',
 	description:
 		'Godot-Router is a routing library for the Godot Game Engine that helps you manage navigation and scene transitions in a structured and maintainable way.'
 } as const;
